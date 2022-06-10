798f0cdd23164d0dcf8ed9237c9e51b8c66b57e4
coding-sta <personal.sta.bd@gmail.com>
2022-06-10 12:25:59 +0600

    button done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./Banner.css";
 import banner from "../../../Assets/Banner/banner.jpg";
-import 'animate.css';
+import "animate.css";
 const Banner = () => {
   return (
     <div className="banner">
       <div className="image">
-    <img src={banner} alt="" />
-  
+        <img src={banner} alt="" />
       </div>
-      
-      <div className=" animate__animated animate__slideInDown details">
-        <h1>Sheikh tofayel ahmed</h1>
+
+      <div className="details">
+        <div className=" animate__animated animate__slideInDown">
+          <h1>Sheikh tofayel ahmed</h1>
+        </div>
+        <h2>web application developer</h2>
+        <div className="button">
+          <button className="btn-1">who i am</button>
+          <button className="btn-2">contact me</button>
+        </div>
       </div>
     </div>
   );
